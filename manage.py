defeae106d173ced6f69bd75dae5c26461ed4d28
alexkarari <k.alexander@gmail.com>
2018-04-18 17:10:35 +0300

    manage.py
    manager shell decorator altered

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import User, Blog, User_comments, Subscription
 
 app = create_app('default')
 
@@ -12,7 +12,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Blog=Blog, User_comments=User_comments, Subscription=Subscription)
 
 if __name__ == '__main__':
     manager.run()
