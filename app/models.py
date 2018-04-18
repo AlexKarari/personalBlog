1de77a4ca4a1f0d1bb4c28e41a8039cd46fe574c
alexkarari <k.alexander@gmail.com>
2018-04-18 17:04:31 +0300

    Models.py
    Tables blog, comments and subscribers created

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.name}'
 
 class Blog(db.Model):
     __tablename__='blog'
@@ -33,6 +33,29 @@ class Blog(db.Model):
     blog_name = db.Column(db.String(100))
     blog_info = db.Column(db.String(20000))
     date_posted = db.Column(db.Time, default=datetime.utcnow())
-    
+    comments = db.relationship('User_comments', backref='comment', lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.blog_name}'
+
+class User_comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    blogID = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Subscription(db.Model):
+    __tablename__ = 'subscribers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
 
 
