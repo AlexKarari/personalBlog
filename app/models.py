e35dd7837594a0aa154fb0b55a9741d15ea7bfdc
alexkarari <k.alexander@gmail.com>
2018-04-18 16:28:42 +0300

    manage.py
    werkzeug security imported
    datetime imported
    Blog table crteated

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class User(db.Model):
     __tablename__='blogger'
@@ -7,5 +9,30 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password_hash = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__='blog'
+    id = db.Column(db.Integer, primary_key=True)
+    blog_name = db.Column(db.String(100))
+    blog_info = db.Column(db.String(20000))
+    date_posted = db.Column(db.Time, default=datetime.utcnow())
+    
+
+
