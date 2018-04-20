8b660d6a8ee752708de99f05e011520b3c64bc84
alexkarari <k.alexander@gmail.com>
2018-04-20 14:00:48 +0300

    Login
    Login authentication created

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,18 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_simplemde import SimpleMDE
+from flask_login import LoginManager
+from flask_mail import Mail
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
+mail = Mail()
+
 
 def create_app(config_state):
     app = Flask(__name__)
@@ -17,8 +25,12 @@ def create_app(config_state):
     bootstrap.init_app(app)
     db.init_app(app)
     simple.init_app(app)
+    mail.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
